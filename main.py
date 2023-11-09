5d0bc0a6e6ee70bef6bb53534b6589e2075acbb6
pesala111 <pesalamadhavareddy1@gmail.com>
2023-11-09 17:28:39 +0100

    updated

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ subjects_list = []   #case_1
 # #checked---tensor os 4-dim array [1,240,240,155]
 #checked---labels are converted to interger format
 
-class_labels = {'HGG': 1, 'LGG': 2}
+class_labels = {'HGG': 0, 'LGG': 1}
 
 preprocessing_transforms = tio.Compose([
     tio.RescaleIntensity(out_min_max=(0, 1)),
     tio.ZNormalization(masking_method=tio.ZNormalization.mean),
-    #tio.Resize((128, 128, 155))
+    #tio.Resize())
     ])
 
 for classes in os.listdir(root_dir):
@@ -47,8 +47,6 @@ dataset_size = len(dataset)
 
 dataset_type = type(dataset)
 print(f"Type of dataset: {dataset_type}")
-
-# Length/Shape of the dataset
 dataset_length = len(dataset)
 print(f"Number of subjects in dataset: {dataset_length}")
 
@@ -68,16 +66,20 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
+
 class ResNet3D(nn.Module):
     def __init__(self, num_classes, dropout_prob=0.5):
         super(ResNet3D, self).__init__()
 
-        # Loading the pre-trained ResNet3D model
-        self.resnet3d = models.video.r3d_18(pretrained=True)
+        self.resnet3d = models.video.r3d_18(pretrained=False)
+
+        # Modify the first convolutional layer to take 1-channel input
+        self.resnet3d.stem[0] = nn.Conv3d(
+            1, 64, kernel_size=(3, 7, 7), stride=(1, 2, 2), padding=(1, 3, 3), bias=False
+        )
 
         num_features = self.resnet3d.fc.in_features
 
-        # adding dropout for regularization
         self.resnet3d.fc = nn.Sequential(
             nn.Dropout(dropout_prob),
             nn.Linear(num_features, num_classes)
@@ -86,10 +88,9 @@ class ResNet3D(nn.Module):
     def forward(self, x):
         return self.resnet3d(x)
 
-num_classes = 2  # Adjust the classes
-model = ResNet3D(num_classes)
 
-model.eval()
+num_classes = 2  # actually the number of classes are 2 but since we are using binary cross entropy so we make it 1
+model = ResNet3D(num_classes)
 
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -111,13 +112,22 @@ best_val_loss = float('inf')
 epochs_no_improve = 0
 
 for epoch in range(num_epochs):
+    print("epoch started")
     model.train()
     running_loss = 0.0
     correct_train = 0
     total_train = 0
 
-    for i, (inputs, labels) in enumerate(train_loader):
-        inputs, labels = inputs.to(device), labels.to(device)
+    for i, batch in enumerate(train_loader):
+        inputs = batch['t1'][tio.DATA].to(device)  # Transfer image data to device
+
+        # Check if 'label' is already a tensor. If it is, simply move it to the device.
+        if torch.is_tensor(batch['label']):
+            labels = batch['label'].to(device, dtype=torch.long)
+        else:
+            # If 'label' is a list or numpy array, convert it to a tensor and then move to the device.
+            labels = torch.tensor(batch['label'], dtype=torch.long).to(device)
+
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, labels)
@@ -138,10 +148,16 @@ for epoch in range(num_epochs):
     running_val_loss = 0.0
     correct_val = 0
     total_val = 0
-
+    print("epochmiddle")
     with torch.no_grad():
-        for i, (inputs, labels) in enumerate(val_loader):
-            inputs, labels = inputs.to(device), labels.to(device)
+        for i, batch in enumerate(val_loader):
+            inputs = batch['t1'][tio.DATA].to(device)  # Transfer image data to device
+
+            # Same handling for validation labels
+            if torch.is_tensor(batch['label']):
+                labels = batch['label'].to(device, dtype=torch.long)
+            else:
+                labels = torch.tensor(batch['label'], dtype=torch.long).to(device)
 
             outputs = model(inputs)
             loss = criterion(outputs, labels)
@@ -158,7 +174,7 @@ for epoch in range(num_epochs):
     val_accuracies.append(val_accuracy)
     print(f"Epoch [{epoch + 1}/{num_epochs}] - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%, Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.2f}%")
 
-    # early stopping
+    # Early stopping
     if val_loss < best_val_loss:
         best_val_loss = val_loss
         epochs_no_improve = 0
@@ -167,4 +183,5 @@ for epoch in range(num_epochs):
         if epochs_no_improve == patience:
             print(f"Early stopping! Best validation loss: {best_val_loss:.4f}")
             break
+
     scheduler.step(val_loss)
